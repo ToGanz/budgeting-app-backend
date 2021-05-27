88660e94f8018005b07a94deb5d821f468c1b820
tganz <tganz5890@googlemail.com>
2021-05-27 15:04:28 +0200

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_091119) do
+ActiveRecord::Schema.define(version: 2021_05_27_130206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2021_05_27_091119) do
     t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["plan_id"], name: "index_transactions_on_plan_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "transactions", "categories"
