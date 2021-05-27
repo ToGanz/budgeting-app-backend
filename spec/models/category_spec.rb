a1f747bb54793d768aee5fd36ee6e031e685d2f3
tganz <tganz5890@googlemail.com>
2021-05-27 11:31:10 +0200

    add category validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:category) }
+
+  context 'all required fields are present' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'name is missing' do
+    it 'is not valid' do
+      subject.update(name: '')
+      expect(subject.errors).to have_key(:name)
+    end
+  end
+
+	context 'name is not unique' do
+    before { Category.create(name: 'Same title') }
+    subject { Category.create(name: 'Same title') }
+
+    it 'is not valid' do
+      expect(subject.errors[:name]).to include('has already been taken')
+    end
+  end
+
+  context 'destroy category should destroy linked transactions' do
+    before { FactoryBot.create(:transaction, category: subject) }
+
+    it 'is not valid' do
+      expect { subject.destroy }.to change { Transaction.count }.by(-1)
+    end
+  end
 end
