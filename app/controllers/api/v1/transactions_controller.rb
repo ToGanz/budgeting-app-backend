5db59f58a676a57788982c058237f9622e441950
tganz <tganz5890@googlemail.com>
2021-05-27 05:44:19 +0200

    add transaction show endpoint

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::TransactionsController < ApplicationController
   before_action :set_plan
+  before_action :set_transaction, only: [:show, :update, :destroy]
 
   def index
     transactions = @plan.transactions
     render json: transactions, status: :ok
   end
 
+  def show
+    render json: @transaction, status: :ok
+  end
+
   private
 
   def set_plan
     @plan = Plan.find(params[:plan_id])
+  end
+
+  def set_transaction
+    @transaction = @plan.transactions.find_by!(id: params[:id]) if @plan
   end
 end
--- spec/requests/api/v1/transactions_spec.rb
+++ spec/requests/api/v1/transactions_spec.rb
@@ -37,7 +37,37 @@ RSpec.describe "Api::V1::Transactions", type: :request do
       end
     end
 
-  end
+  end # end of index
+
+  # show
+  describe "GET /plans/:plan_id/transactions/:id" do
+    before { get "/api//v1/plans/#{plan_id}/transactions/#{id}"}
+
+    context 'when the record exists' do
+      it 'returns the plan' do
+        json_response = JSON.parse(response.body)
+        expect(json_response).not_to be_empty
+        expect(json_response['id']).to eq(id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Transaction/)
+      end
+    end
+
+  end # end of show
 
 
 end
