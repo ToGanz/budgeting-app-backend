b94435642c4d360bebf1a993a36d274baef3f85c
tganz <tganz5890@googlemail.com>
2021-05-28 04:52:33 +0200

    add user validations and bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  validates :email, uniqueness: true 
+  validates_format_of :email, with: /@/ 
+  validates :name, presence: true
+  validates :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:user) }
+
+  context 'all required fields are present' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'name is missing' do
+    it 'is not valid' do
+      subject.update(name: '')
+      expect(subject.errors).to have_key(:name)
+    end
+  end
+
+  context 'password is missing' do
+    it 'is not valid' do
+      subject.update(password_digest: '')
+      expect(subject.errors).to have_key(:password_digest)
+    end
+  end
+
+  context 'email is missing' do
+    it 'is not valid' do
+      subject.update(email: '')
+      expect(subject.errors).to have_key(:email)
+    end
+  end
+
+  context 'email has the wrong format' do
+    it 'is not valid' do
+      subject.update(email: 'test.com')
+      expect(subject.errors).to have_key(:email)
+    end
+  end
+
+  context 'email is not unique' do
+    before { User.create(name: 'Burger', email: 'test@test.com', password: 'password') } 
+    subject { User.create(name: 'Burger 3', email: 'test@test.com', password: 'password') }
+
+    it 'is not valid' do
+      expect(subject.errors[:email]).to include('has already been taken')
+    end 
+  end
 end
