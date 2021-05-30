24a4fd429f6ce2cd05fa05e015d1c61538cc1fc5
tganz <tganz5890@googlemail.com>
2021-05-30 05:01:55 +0200

    setup jwt class

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,18 @@
+class JsonWebToken
+  # secret to encode and decode token
+  SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
+
+  def self.encode(payload, exp = 24.hours.from_now) 
+    # set expiry to 24 hours from creation time
+    payload[:exp] = exp.to_i
+     # sign token with application secret
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    # get payload; first index in decoded Array
+    decoded = JWT.decode(token, SECRET_KEY).first 
+    HashWithIndifferentAccess.new decoded
+    
+  end 
+end
